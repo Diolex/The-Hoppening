0f40bf70e08d72bec5ba011792dc802f2a905244
Jesse Bond <jjb315@lehigh.edu>
2014-03-28 01:01:44 -0400

    Added BunnyActor Animations

--- The_Hoppening/src/com/me/the_hoppening/actors/BunnyActor.java
+++ The_Hoppening/src/com/me/the_hoppening/actors/BunnyActor.java
@@ -26,26 +26,76 @@ public class BunnyActor extends Actor{
 	private boolean mutant = false;
 	private enum State {IDLE_R, RUN_R, ATTACK_R, HIT_R, DEAD_R,
 						IDLE_L, RUN_L, ATTACK_L, HIT_L, DEAD_L,}
-	private State state = State.RUN_R;
+	private State state = State.DEAD_R;
 	private int health = 5;
 	float actorX = 0;
 	float actorY = 0;
 	
 	public BunnyActor(){
 		textureAtlas = new TextureAtlas(Gdx.files.internal("data/basedBunny.atlas"));
-		idleAniR = new Animation(1/15f,
-				(textureAtlas.findRegions("idle0")));
-		runAniR = new Animation(1/15f, textureAtlas.getRegions());
-		attackAniR = new Animation(1/15f, textureAtlas.getRegions());
-		hitAniR = new Animation(1/15f, textureAtlas.getRegions());
-		deadAniR = new Animation(1/15f, textureAtlas.getRegions());
+		idleAniR = new Animation(1/6f,
+				(textureAtlas.findRegion("right/idle0")),
+				(textureAtlas.findRegion("right/idle1")),
+				(textureAtlas.findRegion("right/idle2")),
+				(textureAtlas.findRegion("right/idle3")),
+				(textureAtlas.findRegion("right/idle4")),
+				(textureAtlas.findRegion("right/idle5")));
+		runAniR = new Animation(1/10f,
+				(textureAtlas.findRegion("right/run0")),
+				(textureAtlas.findRegion("right/run1")),
+				(textureAtlas.findRegion("right/run2")),
+				(textureAtlas.findRegion("right/run3")));
+		attackAniR = new Animation(1/6f,
+				(textureAtlas.findRegion("right/attack0")),
+				(textureAtlas.findRegion("right/attack1")),
+				(textureAtlas.findRegion("right/attack2")));
+		hitAniR = new Animation(1/10f,
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")));
+		deadAniR = new Animation(1/10f,
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")));
 		
-		idleAniL = new Animation(1/15f, textureAtlas.getRegions());
-		runAniL = new Animation(1/15f, textureAtlas.getRegions());
-		attackAniL = new Animation(1/15f, textureAtlas.getRegions());
-		hitAniL = new Animation(1/15f, textureAtlas.getRegions());
-		deadAniL = new Animation(1/15f, textureAtlas.getRegions());
-				
+		idleAniL = new Animation(1/6f,
+				(textureAtlas.findRegion("left/idle0")),
+				(textureAtlas.findRegion("left/idle1")),
+				(textureAtlas.findRegion("left/idle2")),
+				(textureAtlas.findRegion("left/idle3")),
+				(textureAtlas.findRegion("left/idle4")),
+				(textureAtlas.findRegion("left/idle5")));
+		runAniL = new Animation(1/10f,
+				(textureAtlas.findRegion("left/run0")),
+				(textureAtlas.findRegion("left/run1")),
+				(textureAtlas.findRegion("left/run2")),
+				(textureAtlas.findRegion("left/run3")));
+		attackAniL = new Animation(1/6,
+				(textureAtlas.findRegion("left/attack0")),
+				(textureAtlas.findRegion("left/attack1")),
+				(textureAtlas.findRegion("left/attack2")));
+		hitAniL = new Animation(1/10f,
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")));
+		deadAniL = new Animation(1/10f,
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("left/hit0")),
+				(textureAtlas.findRegion("right/hit0")),
+				(textureAtlas.findRegion("right/hit0")));
 		setBounds(actorX,actorY,idleAniR.getKeyFrame(elapsedTime,true).getRegionHeight(),
 				idleAniR.getKeyFrame(elapsedTime,true).getRegionWidth());
 	}
@@ -67,22 +117,22 @@ public class BunnyActor extends Actor{
 			frame = runAniL.getKeyFrame(elapsedTime, true);
 			break;
 		case ATTACK_R: 
-			frame = attackAniR.getKeyFrame(elapsedTime, false);
+			frame = attackAniR.getKeyFrame(elapsedTime, true);
 			break;
 		case ATTACK_L: 
-			frame = attackAniL.getKeyFrame(elapsedTime, false);
+			frame = attackAniL.getKeyFrame(elapsedTime, true);
 			break;
 		case HIT_R: 
-			frame = hitAniR.getKeyFrame(elapsedTime, false);
+			frame = hitAniR.getKeyFrame(elapsedTime, true);
 			break;
 		case HIT_L: 
-			frame = hitAniL.getKeyFrame(elapsedTime, false);
+			frame = hitAniL.getKeyFrame(elapsedTime, true);
 			break;
 		case DEAD_R: 
-			frame = deadAniR.getKeyFrame(elapsedTime, false);
+			frame = deadAniR.getKeyFrame(elapsedTime, true);
 			break;
 		default:  
-			frame = deadAniL.getKeyFrame(elapsedTime, false);
+			frame = deadAniL.getKeyFrame(elapsedTime, true);
 			break;
 		}
 
