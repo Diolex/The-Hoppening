bb9c563aa776694cb1c294ea548875d29276d199
Jesse Bond <jjb315@lehigh.edu>
2014-03-27 22:41:27 -0400

    Created MobActor.java and CarrotActor.java
    MobActor is the abstract class for Mobs
    
    CarrotActor inherits from Mobactor

---
+++ The_Hoppening/src/com/me/the_hoppening/actors/CarrotActor.java
@@ -0,0 +1,16 @@
+package com.me.the_hoppening.actors;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class CarrotActor extends MobActor {
+	public CarrotActor(){
+		super("data/carrot.atlas");		
+		
+		
+		idleAni = new Animation(1/15f, textureAtlas.getRegions());
+		//setBounds(actorX,actorY,animation.getKeyFrame(elapsedTime,true).getRegionHeight(),
+		//		animation.getKeyFrame(elapsedTime,true).getRegionWidth());	
+	}
+}
---
+++ The_Hoppening/src/com/me/the_hoppening/actors/MobActor.java
@@ -0,0 +1,72 @@
+package com.me.the_hoppening.actors;
+
+import java.util.Iterator;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Action;
+
+public abstract class MobActor extends Actor{
+	protected Animation idleAni;
+	protected Animation runAni;
+	protected Animation attackAni;
+	protected Animation hitAni;
+	protected Animation deadAni;
+	protected TextureAtlas textureAtlas;
+	protected FileHandle atlasPath;
+	protected float elapsedTime;
+	protected boolean mutant;
+	protected boolean isLeft;
+	protected enum State {IDLE, RUN, ATTACK, HIT, DEAD}
+	protected State state;
+	protected int health;
+	float actorX;
+	float actorY;
+
+	public MobActor(String path){
+		elapsedTime = 0;
+		mutant = false;
+		isLeft = false;
+		state = State.IDLE;
+		health = 5;
+		actorX = 0;
+		actorY = 0;
+		textureAtlas = new TextureAtlas(Gdx.files.internal(path));
+	}
+	
+	@Override
+	public void draw(SpriteBatch batch, float alpha){
+		TextureRegion frame;
+		switch (state){
+		case IDLE: //TODO
+			frame = idleAni.getKeyFrame(elapsedTime, true);
+		case RUN: //TODO
+			frame = runAni.getKeyFrame(elapsedTime, true);
+		case ATTACK: //TODO
+			frame = attackAni.getKeyFrame(elapsedTime, false);
+		case HIT: //TODO
+			frame = hitAni.getKeyFrame(elapsedTime, false);
+		default:  //TODO DEAD
+			frame = deadAni.getKeyFrame(elapsedTime, false);
+		}
+		//if(isLeft) frame.flip(true,false);
+		batch.draw(frame, actorX, actorY);
+	}
+	
+	@Override
+	public void act(float delta){
+		elapsedTime += delta;
+		for(Iterator<Action> iter = this.getActions().iterator(); iter.hasNext();){
+	        iter.next().act(delta);
+	    }
+	}
+	
+	public int getHealth(){
+		return health;
+	}
+}
