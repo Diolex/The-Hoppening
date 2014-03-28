7492b683b46f8ed043dea0367c1f1c33d0978aac
Jesse Bond <jjb315@lehigh.edu>
2014-03-28 12:13:06 -0400

    ProjectileACtor added draw and act

--- The_Hoppening/src/com/me/the_hoppening/actors/ProjectileActor.java
+++ The_Hoppening/src/com/me/the_hoppening/actors/ProjectileActor.java
@@ -1,15 +1,19 @@
 package com.me.the_hoppening.actors;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public abstract class ProjectileActor extends Actor {
 	protected TextureAtlas textureAtlas;
 	protected TextureRegion stable;
 	protected Animation unstable;
-	protected 
+	protected boolean isStable;
 	
 	float actorX;
 	float actorY;
@@ -20,7 +24,17 @@ public abstract class ProjectileActor extends Actor {
 		actorY = y;
 	}
 	
-	@Override void draw(SpriteBatch batch, float alpha){
-		batch.draw
+	@Override
+	public void draw(SpriteBatch batch, float alpha){
+
 	}
+	
+	@Override
+	public void act(float delta){
+		for(Iterator<Action> iter = this.getActions().iterator(); iter.hasNext();){
+	        iter.next().act(delta);
+	    }
+	}
+	
+	
 }
