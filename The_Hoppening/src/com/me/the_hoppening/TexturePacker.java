c1abc9b9691340ba5af1ffc0200c3cc8fe154596
Jesse Bond <jjb315@lehigh.edu>
2014-03-28 00:38:17 -0400

    Updated texturePacker script

--- The_Hoppening/src/com/me/the_hoppening/TexturePacker.java
+++ The_Hoppening/src/com/me/the_hoppening/TexturePacker.java
@@ -4,6 +4,12 @@ import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 public class TexturePacker {
     public static void main (String[] args) throws Exception {
-        TexturePacker2.process("assets/unpacked/", "assets/", "basedBunny");
+    	TexturePacker2.process("assets/unpacked/bunny/", "assets/", "basedBunny");
+    	TexturePacker2.process("assets/unpacked/bunny/", "../The_Hoppening-desktop/assets/data/", "basedBunny");
+    	TexturePacker2.process("assets/unpacked/bunny/", "../The_Hoppening-android/assets/data/", "basedBunny");
+    	TexturePacker2.process("assets/unpacked/mutantbunny/", "assets/", "basedMutantBunny");
+    	TexturePacker2.process("assets/unpacked/mutantbunny/", "../The_Hoppening-desktop/assets/data/", "basedMutantBunny");
+    	TexturePacker2.process("assets/unpacked/mutantbunny/", "../The_Hoppening-android/assets/data/", "basedMutantBunny");
+
     }
 }
