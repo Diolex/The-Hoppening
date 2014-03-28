4cdca05ee7dfc8b6ff09c1a608c8d86428be6c08
Jesse Bond <jjb315@lehigh.edu>
2014-03-28 00:26:07 -0400

    fixed the flipping error in BunnyActor

--- The_Hoppening/src/com/me/the_hoppening/actors/BunnyActor.java
+++ The_Hoppening/src/com/me/the_hoppening/actors/BunnyActor.java
@@ -11,54 +11,80 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Action;
 
 public class BunnyActor extends Actor{
-	private Animation idleAni;
-	private Animation runAni;
-	private Animation attackAni;
-	private Animation hitAni;
-	private Animation deadAni;
+	private Animation idleAniR;
+	private Animation runAniR;
+	private Animation attackAniR;
+	private Animation hitAniR;
+	private Animation deadAniR;
+	private Animation idleAniL;
+	private Animation runAniL;
+	private Animation attackAniL;
+	private Animation hitAniL;
+	private Animation deadAniL;
 	private TextureAtlas textureAtlas;
 	private float elapsedTime = 0;
 	private boolean mutant = false;
-	private boolean flipIt = false;
-	private enum State {IDLE, RUN, ATTACK, HIT, DEAD}
-	private State state = State.RUN;
+	private enum State {IDLE_R, RUN_R, ATTACK_R, HIT_R, DEAD_R,
+						IDLE_L, RUN_L, ATTACK_L, HIT_L, DEAD_L,}
+	private State state = State.RUN_R;
 	private int health = 5;
 	float actorX = 0;
 	float actorY = 0;
 	
 	public BunnyActor(){
 		textureAtlas = new TextureAtlas(Gdx.files.internal("data/basedBunny.atlas"));
-		idleAni = new Animation(1/15f, textureAtlas.getRegions());
-		runAni = new Animation(1/5f, textureAtlas.getRegions());
-		attackAni = new Animation(1/15f, textureAtlas.getRegions());
-		hitAni = new Animation(1/15f, textureAtlas.getRegions());
-		deadAni = new Animation(1/15f, textureAtlas.getRegions());
-		setBounds(actorX,actorY,idleAni.getKeyFrame(elapsedTime,true).getRegionHeight(),
-				idleAni.getKeyFrame(elapsedTime,true).getRegionWidth());
+		idleAniR = new Animation(1/15f, textureAtlas.getRegions());
+		runAniR = new Animation(1/15f, textureAtlas.getRegions());
+		attackAniR = new Animation(1/15f, textureAtlas.getRegions());
+		hitAniR = new Animation(1/15f, textureAtlas.getRegions());
+		deadAniR = new Animation(1/15f, textureAtlas.getRegions());
+		
+		idleAniL = new Animation(1/15f, textureAtlas.getRegions());
+		runAniL = new Animation(1/15f, textureAtlas.getRegions());
+		attackAniL = new Animation(1/15f, textureAtlas.getRegions());
+		hitAniL = new Animation(1/15f, textureAtlas.getRegions());
+		deadAniL = new Animation(1/15f, textureAtlas.getRegions());
+				
+		setBounds(actorX,actorY,idleAniR.getKeyFrame(elapsedTime,true).getRegionHeight(),
+				idleAniR.getKeyFrame(elapsedTime,true).getRegionWidth());
 	}
 	
 	@Override
 	public void draw(SpriteBatch batch, float alpha){
 		TextureRegion frame;
 		switch (state){
-		case IDLE: //TODO
-			frame = idleAni.getKeyFrame(elapsedTime, true);
+		case IDLE_R: 
+			frame = idleAniR.getKeyFrame(elapsedTime, true);
 			break;
-		case RUN: //TODO
-			frame = runAni.getKeyFrame(elapsedTime, true);
+		case IDLE_L: 
+			frame = idleAniL.getKeyFrame(elapsedTime, true);
 			break;
-		case ATTACK: //TODO
-			frame = attackAni.getKeyFrame(elapsedTime, false);
+		case RUN_R: 
+			frame = runAniR.getKeyFrame(elapsedTime, true);
 			break;
-		case HIT: //TODO
-			frame = hitAni.getKeyFrame(elapsedTime, false);
+		case RUN_L: 
+			frame = runAniL.getKeyFrame(elapsedTime, true);
 			break;
-		default:  //TODO DEAD
-			frame = deadAni.getKeyFrame(elapsedTime, false);
+		case ATTACK_R: 
+			frame = attackAniR.getKeyFrame(elapsedTime, false);
+			break;
+		case ATTACK_L: 
+			frame = attackAniL.getKeyFrame(elapsedTime, false);
+			break;
+		case HIT_R: 
+			frame = hitAniR.getKeyFrame(elapsedTime, false);
+			break;
+		case HIT_L: 
+			frame = hitAniL.getKeyFrame(elapsedTime, false);
+			break;
+		case DEAD_R: 
+			frame = deadAniR.getKeyFrame(elapsedTime, false);
+			break;
+		default:  
+			frame = deadAniL.getKeyFrame(elapsedTime, false);
 			break;
 		}
-		//if(isLeft) 
-			frame.flip(true,false);
+
 		batch.draw(frame, actorX, actorY);
 	}
 	
